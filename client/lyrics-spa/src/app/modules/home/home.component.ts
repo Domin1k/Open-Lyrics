7762f7e9e85a8750e68dabbc8acc1ef725cf1605
Kristian Lyubenov <kristian.lubenov@gmail.com>
2020-02-22 10:14:09 +0200

    Angular client - added animations

--- client/lyrics-spa/src/app/modules/home/home.component.ts
+++ client/lyrics-spa/src/app/modules/home/home.component.ts
@@ -1,17 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 import { ActivatedRoute } from '@angular/router';
 import { LyricResponseModel } from 'src/app/shared/models/lyric/lyric-response.model';
 import { LyricService } from 'src/app/core/services/lyric.service';
 import { AllLyricsRequestModel } from 'src/app/shared/models/lyric/all-lyrics-request.model';
 import { AllLyricsResponseModel } from 'src/app/shared/models/lyric/all-lyrics-response.model';
+import { trigger, transition, animate, style, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('pageAnimations', [
+      transition(':enter', [
+        query('.all-lyrics', [
+          style({opacity: 0, transform: 'translateY(-100px)'}),
+          stagger(-30, [
+            animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1, transform: 'none' }))
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
+  @HostBinding('@pageAnimations')
+  public animatePage = true;
+  
   length: number;
   pageSize = 10;
   pageSizeOptions: number[] = [5, 10, 25, 100];
--- client/lyrics-spa/src/app/modules/lyric/my/my.component.ts
+++ client/lyrics-spa/src/app/modules/lyric/my/my.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { PageEvent } from '@angular/material';
 import { LyricResponseModel } from 'src/app/shared/models/lyric/lyric-response.model';
 import { ActivatedRoute } from '@angular/router';
@@ -6,13 +6,29 @@ import { LyricService } from 'src/app/core/services/lyric.service';
 import { MyLyricsRequestModel } from 'src/app/shared/models/lyric/my-lyrics-request.model';
 import { MyLyricsResponseModel } from 'src/app/shared/models/lyric/my-lyrics-response.model';
 import { LyricDetailsResponseModel } from 'src/app/shared/models/lyric/details-lyric-response.model';
+import { trigger, transition, animate, style, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-my',
   templateUrl: './my.component.html',
-  styleUrls: ['./my.component.css']
+  styleUrls: ['./my.component.css'],
+  animations: [
+    trigger('pageAnimations', [
+      transition(':enter', [
+        query('.my-lyrics', [
+          style({opacity: 0, transform: 'translateY(-100px)'}),
+          stagger(-30, [
+            animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({ opacity: 1, transform: 'none' }))
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class MyComponent implements OnInit {
+  @HostBinding('@pageAnimations')
+  public animatePage = true;
+  
   length: number;
   pageSize = 10;
   pageSizeOptions: number[] = [5, 10, 25, 100];
